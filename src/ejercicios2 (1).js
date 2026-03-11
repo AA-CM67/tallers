bc6e9a3ace691059de2836de68688d5e2ef0f377
AA-CM67 <you@example.com>
2026-03-11 14:06:30 -0500

    feat:ejercicio test 2

--- src/ejercicios2 (1).js
+++ src/ejercicios2 (1).js
@@ -18,7 +18,12 @@ function esMayorDeEdad(edad) {return edad >=18;
 }
 
 // Test 2:
-function clasificarTriangulo(lado1, lado2, lado3) {
+function clasificarTriangulo(lado1, lado2, lado3) {f (lado1 + lado2 <= lado3 || lado1 + lado3 <= lado2 || lado2 + lado3 <= lado1) {
+    return "No es triángulo";
+  }
+  if (lado1 === lado2 && lado2 === lado3) return "Equilátero";
+  if (lado1 === lado2 || lado1 === lado3 || lado2 === lado3) return "Isósceles";
+  return "Escaleno";
 	// Recibe tres números representando los lados de un triángulo.
 	// Devuelve:
 	// - "Equilátero" si los tres lados son iguales
